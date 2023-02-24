0d7a5f52a7c68cb151706e1911d7ac289a86c3c3
Nick White <bbycinka@yandex.ru>
2023-02-24 17:56:39 +0300

    refact:(main): location

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styleApp from "./App.module.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import cn from "classnames";
 import { MainPage } from "../../pages/MainPage/MainPage";
 import { Header } from "../Header/Header";
 import { Menu } from "../Menu/Menu";
 import { HardsPage } from "../../pages/HardsPage/HardsPage";
 export const App = () => {
+  const location = useLocation()
+  const background = location.state && location?.state?.background
   return (
     <div className={cn(styleApp.div, "container")}>
       <Header />
       <div className={styleApp.main}>
         <Menu />
-        <Routes>
+        <Routes location={background || location}>
           <Route path="/" element={<MainPage />} />
           <Route path="/hards" element={<HardsPage />} />
           <Route path="*" element={<h1>Error</h1>} />
