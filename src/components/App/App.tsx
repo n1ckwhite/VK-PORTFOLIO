c6bb73d3fd25f332480c98e28ca4a23e8c85b704
Nick White <bbycinka@yandex.ru>
2023-02-24 18:29:43 +0300

    fet:(main): добавал hashrouter

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styleApp from "./App.module.css";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, HashRouter, useLocation } from "react-router-dom";
 import cn from "classnames";
 import { MainPage } from "../../pages/MainPage/MainPage";
 import { Header } from "../Header/Header";
@@ -14,11 +14,13 @@ export const App = () => {
       <Header />
       <div className={styleApp.main}>
         <Menu />
+        <HashRouter>
         <Routes location={background || location}>
           <Route path="/" element={<MainPage />} />
           <Route path="/hards" element={<HardsPage />} />
           <Route path="*" element={<h1>Error</h1>} />
         </Routes>
+        </HashRouter>
       </div>
     </div>
   );
